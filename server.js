91cc296bdd705dc1bd4494af62d85bb519b3c6fd
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-03-31 11:30:27 +0300

    change default port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const httpPort = process.env.PORT || 3000;
+const httpPort = process.env.PORT || 8080;
 const services = require('./domain')();
 const app = require('./router/http/app')(services);
 
