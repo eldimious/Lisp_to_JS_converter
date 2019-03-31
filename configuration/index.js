af5602287fb6254c494774b85f5a2764eeb14577
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-03-31 11:32:05 +0300

    Add config

---
+++ configuration/index.js
@@ -0,0 +1,12 @@
+/**
+  * This module is used to collect all the configuration variables,
+  * like the environment vars, in one place so they are not scattered all over the whole codebase
+*/
+require('dotenv').config();
+
+
+const config = {
+  httpPort: process.env.HTTP_PORT || 8080,
+};
+
+module.exports = config;
